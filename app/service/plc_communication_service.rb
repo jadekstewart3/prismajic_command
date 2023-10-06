6085cf9925a5e180350436fca9a53d5947e2ef7d
Jade Stewart <jadekstewart3@gmail.com>
2023-10-06 16:19:21 -0600

    Edit service to be initialized

--- app/service/plc_communication_service.rb
+++ app/service/plc_communication_service.rb
@@ -1,13 +1,20 @@
 require "socket"
+class PlcCommunicationService
 
-class PLCCommunicationService
+  attr_reader :plc_ip, :port_number
 
-  def self.send_udp_command(plc_port, udp_command)
+  def initialize(plc_ip, port_number)
+    @plc_ip = plc_ip
+    @port_number = port_number
+  end
+
+  def self.send_udp_command(udp_command)
     udp_socket = UDPSocket.new(Socket::AF_INET)
-    begin
-      udp_socket.send(udp_command, 0, plc_port.ip_address, plc_port.port)
 
-      puts "Command sent successfully to #{plc_port.ip_address}:#{plc_port.port}: #{udp_command}!"
+    begin
+      udp_socket.send(udp_command, 0, @plc_ip, @port_number)
+
+      puts "Command sent successfully to #{@plc_ip}:#{@plc_port}: #{udp_command}!"
     rescue => e
       puts "Error #{e.message}"
     ensure
