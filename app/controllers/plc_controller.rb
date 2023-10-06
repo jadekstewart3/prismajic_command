9620fefe8d9b8a66e8472ef787e048b6ed0e775b
Jade Stewart <jadekstewart3@gmail.com>
2023-10-06 16:18:32 -0600

    Adjust send_udp_command_to_plc to utilize params coming from form

--- app/controllers/plc_controller.rb
+++ app/controllers/plc_controller.rb
@@ -1,15 +1,20 @@
+require "socket"
+
 class PlcController < ApplicationController
 
   def show
-    PlcPort.find(params[:id])
+    @plc = Plc.find(params[:id])
+    @ports = @plc.ports
   end
-  #make a show page for this plc that the form lives on
+ 
 
   def send_udp_command_to_plc
-    plc_port = PlcPort.find(params[:plc_port_id])
+    require 'pry'; binding.pry
+    plc = Plc.find(params[:plc_id])
+    plc_port = Port.find(params[:port_id])
     udp_command = params[:udp_command]
 
-    PLCCommunicationService.send_udp_command(plc_port, udp_command)
+    PlcCommunicationService.new(plc.ip_address, plc_port.port_number).send_udp_command(udp_command)
 
     #redirect to plc show flash[:success] = "Command send successfully!"
   end
