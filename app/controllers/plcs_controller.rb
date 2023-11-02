18c7cf03b950537b43e47ba40777fb5e4cbdcc71
Jade Stewart <jadekstewart3@gmail.com>
2023-11-02 11:48:24 -0600

    Error handle send upd command

--- app/controllers/plcs_controller.rb
+++ app/controllers/plcs_controller.rb
@@ -15,7 +15,6 @@ class PlcsController < ApplicationController
     plc = plc_params
     new_plc = Plc.create(plc)
     if new_plc.save
-
       redirect_to plc_path(new_plc)
     else
       flash[:error] = new_plc.errors.full_messages.join(", ")
@@ -25,15 +24,21 @@ class PlcsController < ApplicationController
 
   #Future Jade: this is where error handling needs to happen
   def send_udp_command_to_plc
-    plc = Plc.find(params[:plc_id])
-    plc_port = Port.find(params[:port_id])
-    udp_command = params[:udp_command]
+    begin
+      plc = Plc.find(params[:plc_id])
+      plc_port = Port.find(params[:port_id])
+      udp_command = params[:udp_command]
 
-    plc_service = PlcCommunicationService.new(plc.ip_address, plc_port.port_number)
-    plc_service.send_udp_command(udp_command)
+      plc_service = PlcCommunicationService.new(plc.ip_address, plc_port.port_number)
+      plc_service.send_udp_command(udp_command)
 
+      flash[:success] = "Command sent successfully!"
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Please select a port."
+    rescue StandardError => e
+      flash[:error] = "An error occurred while sending the command: #{e.message}"
+    end
     redirect_to plc_path(plc.id) 
-    flash[:success] = "Command sent successfully!"
   end
 
   private
