b759712a452f17813b4b658b4369664b4240cebc
Jade Stewart <jadekstewart3@gmail.com>
2023-12-01 14:06:39 -0700

    Additional association tests

--- spec/models/port_spec.rb
+++ spec/models/port_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Port, type: :model do
   describe "relationships" do 
     it { should belong_to :plc }
+    it { should have_many :coils }
   end
 
   describe "validations" do 
