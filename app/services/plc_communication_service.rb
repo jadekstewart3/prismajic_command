5edc92d090ef01cf128039356f876448b5b4ab87
Jade Stewart <jadekstewart3@gmail.com>
2023-12-01 14:02:56 -0700

    Add note to self

--- app/services/plc_communication_service.rb
+++ app/services/plc_communication_service.rb
@@ -1,18 +1,21 @@
 require "socket"
 class PlcCommunicationService
 
-  attr_reader :plc_ip, :port_number
+  attr_reader :plc_ip, :port_number, :address
 
-  def initialize(plc_ip, port_number)
+  def initialize(plc_ip, port_number, address)
     @plc_ip = plc_ip
     @port_number = port_number
-    @udp_socket = TCPSocket.new(@plc_ip, @port_number)
+    @address = address
   end
 
   def send_modbus_command(modbus_command)
-    modbus_client = ModBus::TCPClient.new(@plc_ip, @port_number)
+    require 'pry'; binding.pry
+    modbus_client = ModBus::TCPClient.new(@plc_ip, @port_number, @address)
     response = modbus_client.read_holding_registers(modbus_command[:address], modbus_command[:quantity])
+    # look at docs for this method cause you are not calling this with the correct params
     modbus_client.close
     response
   end
 end
+
